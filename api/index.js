e2a916929efe9b5e4ea734b2ce008fd7138a2d9c
Paras Kanjiya <kanjiyaparas2002@gmail.com>
2024-10-03 00:18:31 +0530

    url update

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "https://airbnb-clone-main-eta.vercel.app/" || "*",
     credentials: true,
   })
 );
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://airbnb-yfut.onrender.com/",
   withCredentials: true,
 });
 
