9cb7052ad4d0bf2fc8f4eb233fd31e08fae45739
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-16 00:32:29 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://airbnb-yfut.onrender.com/",
+  baseURL: "https://airbnb-yfut.onrender.com",
   withCredentials: true,
 });
 
